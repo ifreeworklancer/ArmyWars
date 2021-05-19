552090272b336b3185cf51cee1197e517a397209
Alex Yakushov <yakushov_as@groupbwt.com>
2021-05-19 17:45:58 +0300

    feat: added game step
    Added first step: building barrier, and step two: car Movement

--- src/js/gameScene.ts
+++ src/js/gameScene.ts
@@ -1,10 +1,11 @@
 import "phaser";
 
 export class GameScene extends Phaser.Scene {
-    walls: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
-    info: Phaser.GameObjects.Text;
-    target = new Phaser.Math.Vector2();
+    walls: Phaser.Physics.Arcade.StaticGroup;
     player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    target = new Phaser.Math.Vector2();
+    info: Phaser.GameObjects.Text;
+    isBuildingBarrier: boolean = true;
 
     constructor() {
         super({
@@ -17,21 +18,26 @@ export class GameScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.spritesheet('car', 'assets/sprites/car.png', {frameWidth: 100, frameHeight: 65})
+        this.load.spritesheet('car', 'assets/sprites/car.png', {frameWidth: 80, frameHeight: 65})
         this.load.image('walls', 'assets/sprites/walls.png')
     }
 
     create(): void {
-        this.anims.create({
-            key: "turnCarToRight",
-            frameRate: 25,
-            frames:  this.anims.generateFrameNumbers("car", { start: 0, end: 8 }),
-            repeat: 0
+        this.player = this.physics.add.sprite(100, 500, 'car', 0);
+        this.walls = this.physics.add.staticGroup();
+
+        this.physics.add.collider(this.walls, this.player, () => {
+            this.player.body.reset(this.player.x, this.player.y);
         });
-        this.player = this.physics.add.sprite(100, 500, 'car');
-        this.walls = this.physics.add.image(200, 250, 'walls');
-        this.player.play('turnCarToRight')
-        this.input.on('pointerup', this.carMovement.bind(this))
+
+        this.input.on('pointerup', this.handleClick, this);
+
+        this.info = this.add.text(10, 10, 'Click anywhere in the area to create an barrier\n\nAfter you add all the barrier you need, press space to add a route', {font: '16px Arial Bold'});
+
+        this.input.keyboard.addKey('SPACE').once('down', () => {
+            this.isBuildingBarrier = false
+            this.info.setText('Click anywhere in the area to plot a route for the car')
+        });
     }
 
     update(time): void {
@@ -46,9 +52,28 @@ export class GameScene extends Phaser.Scene {
         }
     }
 
-    carMovement(pointer) {
+    handleClick(pointer) {
         this.target.x = pointer.x;
         this.target.y = pointer.y;
+        if (this.isBuildingBarrier) {
+            this.buildingBarrier()
+            return
+        }
+        this.carMovement()
+    }
+
+    carMovement() {
         this.physics.moveToObject(this.player, this.target, 200);
+    }
+
+    buildingBarrier() {
+        this.walls.create(this.target.x, this.target.y, 'walls').setInteractive();
+        this.walls.getChildren().forEach(wall => {
+            wall.on('pointerup', () => {
+                if (this.isBuildingBarrier) {
+                    wall.destroy()
+                }
+            })
+        })
     }
 }
